c2de76a225dd6b4cfef43015cd1e9e2d60224cd0
Yama12-S <86104884+Yama12-S@users.noreply.github.com>
2021-10-12 10:22:28 +0200

    Projet_Final_db

---
+++ Dump20211012.sql
@@ -0,0 +1,312 @@
+CREATE DATABASE  IF NOT EXISTS `projet_final_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `projet_final_db`;
+-- MySQL dump 10.13  Distrib 8.0.26, for Win64 (x86_64)
+--
+-- Host: localhost    Database: projet_final_db
+-- ------------------------------------------------------
+-- Server version	8.0.26
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `adresse`
+--
+
+DROP TABLE IF EXISTS `adresse`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `adresse` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `numero` varchar(4) NOT NULL,
+  `rue` varchar(45) NOT NULL,
+  `ville` varchar(25) NOT NULL,
+  `codePostal` varchar(5) NOT NULL,
+  `user_id` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `adresse`
+--
+
+LOCK TABLES `adresse` WRITE;
+/*!40000 ALTER TABLE `adresse` DISABLE KEYS */;
+/*!40000 ALTER TABLE `adresse` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `article`
+--
+
+DROP TABLE IF EXISTS `article`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `article` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nom` varchar(30) NOT NULL,
+  `description` varchar(500) NOT NULL,
+  `prix` float NOT NULL,
+  `remise` float NOT NULL,
+  `stock` varchar(45) NOT NULL,
+  `is_salable` bit(1) DEFAULT NULL,
+  `photos` varchar(45) DEFAULT NULL,
+  `videos` varchar(45) DEFAULT NULL,
+  `commentaires` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `article`
+--
+
+LOCK TABLES `article` WRITE;
+/*!40000 ALTER TABLE `article` DISABLE KEYS */;
+/*!40000 ALTER TABLE `article` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `article_panier`
+--
+
+DROP TABLE IF EXISTS `article_panier`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `article_panier` (
+  `id` int NOT NULL,
+  `article` varchar(45) NOT NULL,
+  `quantite` varchar(45) NOT NULL,
+  `utilisateur` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `article_panier`
+--
+
+LOCK TABLES `article_panier` WRITE;
+/*!40000 ALTER TABLE `article_panier` DISABLE KEYS */;
+/*!40000 ALTER TABLE `article_panier` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `carte_paiement`
+--
+
+DROP TABLE IF EXISTS `carte_paiement`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `carte_paiement` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nom_proprietaire` varchar(10) NOT NULL,
+  `numero` varchar(45) DEFAULT NULL,
+  `date_fin_validite` varchar(45) DEFAULT NULL,
+  `cryptogramme` varchar(45) DEFAULT NULL,
+  `prenom_proprietaire` varchar(45) NOT NULL,
+  `utilisateur` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `carte_paiement`
+--
+
+LOCK TABLES `carte_paiement` WRITE;
+/*!40000 ALTER TABLE `carte_paiement` DISABLE KEYS */;
+/*!40000 ALTER TABLE `carte_paiement` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `categorie`
+--
+
+DROP TABLE IF EXISTS `categorie`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `categorie` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nom` varchar(45) NOT NULL,
+  `remise` varchar(45) DEFAULT NULL,
+  `is_remise_cumulable` varchar(45) NOT NULL,
+  `description` varchar(45) NOT NULL,
+  `photo` varchar(45) NOT NULL,
+  `articles` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categorie`
+--
+
+LOCK TABLES `categorie` WRITE;
+/*!40000 ALTER TABLE `categorie` DISABLE KEYS */;
+/*!40000 ALTER TABLE `categorie` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `commande`
+--
+
+DROP TABLE IF EXISTS `commande`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `commande` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `numero` varchar(45) NOT NULL,
+  `date_creation` date NOT NULL,
+  `date-livraison` date NOT NULL,
+  `total-remise` varchar(45) NOT NULL,
+  `frais-expedition` varchar(45) NOT NULL,
+  `total_general` varchar(45) NOT NULL,
+  `adresse_facturation` varchar(45) NOT NULL,
+  `adresse_livraison` varchar(45) NOT NULL,
+  `carte_paiement-default` varchar(45) NOT NULL,
+  `utilisateur` varchar(45) NOT NULL,
+  `lignes_commande` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `commande`
+--
+
+LOCK TABLES `commande` WRITE;
+/*!40000 ALTER TABLE `commande` DISABLE KEYS */;
+/*!40000 ALTER TABLE `commande` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `commentaire`
+--
+
+DROP TABLE IF EXISTS `commentaire`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `commentaire` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `texte` varchar(45) NOT NULL,
+  `note` varchar(45) DEFAULT NULL,
+  `article` varchar(45) DEFAULT NULL,
+  `utilisateur` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `commentaire`
+--
+
+LOCK TABLES `commentaire` WRITE;
+/*!40000 ALTER TABLE `commentaire` DISABLE KEYS */;
+/*!40000 ALTER TABLE `commentaire` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ligne_de_commande`
+--
+
+DROP TABLE IF EXISTS `ligne_de_commande`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ligne_de_commande` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `quantite` varchar(45) NOT NULL,
+  `prix_unitaire` varchar(45) NOT NULL,
+  `remise_article` varchar(45) NOT NULL,
+  `commande` varchar(45) NOT NULL,
+  `article` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ligne_de_commande`
+--
+
+LOCK TABLES `ligne_de_commande` WRITE;
+/*!40000 ALTER TABLE `ligne_de_commande` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ligne_de_commande` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `params`
+--
+
+DROP TABLE IF EXISTS `params`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `params` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `cle_cryptage_pwd` varchar(45) DEFAULT NULL,
+  `cle_cryptage_cp` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `params`
+--
+
+LOCK TABLES `params` WRITE;
+/*!40000 ALTER TABLE `params` DISABLE KEYS */;
+/*!40000 ALTER TABLE `params` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `utilisateur`
+--
+
+DROP TABLE IF EXISTS `utilisateur`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `utilisateur` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nom` varchar(45) NOT NULL,
+  `prenom` varchar(45) NOT NULL,
+  `date_naissance` date NOT NULL,
+  `profil` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `telephone` varchar(45) NOT NULL,
+  `adresses` varchar(45) NOT NULL,
+  `commandes` varchar(45) NOT NULL,
+  `cartes_de_paiement` varchar(45) NOT NULL,
+  `commentaires` varchar(45) NOT NULL,
+  `panier` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `utilisateur`
+--
+
+LOCK TABLES `utilisateur` WRITE;
+/*!40000 ALTER TABLE `utilisateur` DISABLE KEYS */;
+/*!40000 ALTER TABLE `utilisateur` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-10-12 10:19:36

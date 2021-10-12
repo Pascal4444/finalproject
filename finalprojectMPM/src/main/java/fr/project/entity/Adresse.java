0d66020fe026ef0014a69298614b5d2c4c5ced3d
mosab ben <benamaramosab@yahoo.fr>
2021-10-12 09:27:23 +0200

    push mosab adresse

--- finalprojectMPM/src/main/java/fr/project/entity/Adresse.java
+++ finalprojectMPM/src/main/java/fr/project/entity/Adresse.java
@@ -1,10 +1,7 @@
 package fr.project.entity;
 
 public class Adresse {
-
-	public Adresse() {
-		// TODO Auto-generated constructor stub
-	}
+}
 	public Adresse(Integer numero, String rue, String ville, String codePostal) {
 		this.numero = numero;
 		this.rue = rue;

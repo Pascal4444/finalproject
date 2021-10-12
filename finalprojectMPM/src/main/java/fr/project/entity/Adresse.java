eaccda061982943555b84b7edb105fd0cd332410
mosab ben <benamaramosab@yahoo.fr>
2021-10-12 09:26:08 +0200

    first Moss Commit

--- finalprojectMPM/src/main/java/fr/project/entity/Adresse.java
+++ finalprojectMPM/src/main/java/fr/project/entity/Adresse.java
@@ -5,5 +5,11 @@ public class Adresse {
 	public Adresse() {
 		// TODO Auto-generated constructor stub
 	}
+	public Adresse(Integer numero, String rue, String ville, String codePostal) {
+		this.numero = numero;
+		this.rue = rue;
+		this.ville = ville;
+		this.codePostal = codePostal;
+	}
 
 }

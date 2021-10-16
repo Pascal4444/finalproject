5709aee8cd2ed8bdf36c4cb224ee48c3a18caade
Yama12-S <86104884+Yama12-S@users.noreply.github.com>
2021-10-16 15:07:32 +0200

    Add files via upload
    eBoutique-wr-rest-jersey

---
+++ ProjetFinal-eBoutique-wr-rest-jersey/server/CustomApplication.java
@@ -0,0 +1,19 @@
+package fr.doranco.eboutique.ws.rest.jersey.server;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.glassfish.jersey.logging.LoggingFeature;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import fr.doranco.eboutique.ws.rest.jersey.server.provider.AuthenticationFilter;
+
+public class CustomApplication extends ResourceConfig {
+
+	public CustomApplication() {
+		packages("fr.doranco.rest.jersey.server.service");
+		register(new LoggingFeature(Logger.getLogger(LoggingFeature.DEFAULT_LOGGER_NAME), Level.INFO,
+				LoggingFeature.Verbosity.PAYLOAD_ANY, 10000));
+		register(AuthenticationFilter.class);
+	}
+}
